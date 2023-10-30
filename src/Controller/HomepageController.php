8e83a2dd71c07e4387df507e2549d279abd37f19
Iusiel <iusiel@outlook.com>
2023-10-30 17:47:25 +0800

    Initial listings page

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,8 +11,18 @@ class HomepageController extends AbstractController
     #[Route("/", name: "app_homepage")]
     public function index(): Response
     {
+        $listings = [
+            [
+                "title" => "Petite Vue",
+                "description" =>
+                    "From what I understand, you can use petite vue if you do not want to turn your website into a full blown Vue application and just use some Vue features into your website.",
+                "websiteLink" => "https://github.com/vuejs/petite-vue",
+                "testPageLink" => "#",
+            ],
+        ];
+
         return $this->render("homepage/index.html.twig", [
-            "controller_name" => "HomepageController",
+            "listings" => $listings,
         ]);
     }
 }
